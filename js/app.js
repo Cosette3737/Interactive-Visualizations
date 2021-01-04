c82c48cda7ab09d050c7aad407d38b4e8713b22f
Cosette37 <69123930+Cosette37@users.noreply.github.com>
2021-01-04 17:30:52 -0600

    .

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
-function createPlots() {
-    //read json
-    d3.json("samples.json").then((importedData) => {
-
-        console.log(importedData);
-   
-    });}
+d3.json("samples.json").then((Sampledata) => {
+    //filter to get names
+    var parsedData = Sampledata.names;
+    console.log(parsedData);
+    var parseddata=Sampledata.names;
+    //put names into drowdown menu 
+    var dropdown= d3.select("#selDataset");
+    parseddata.forEach((name)=> {
+    dropdown.append("option").property("value", name).text(name);})
+    createPlots(parseddata[0]);
+    createMeta(parseddata[0]);
+    });
