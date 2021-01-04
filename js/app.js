13b5978d07c7f581cbc05e716596f1b75373e786
Cosette37 <69123930+Cosette37@users.noreply.github.com>
2021-01-03 21:43:47 -0600

    .

---
+++ js/app.js
@@ -0,0 +1,15 @@
+//optionchange function to update page
+function optionChanged(newSelection) {
+    createMeta(newSelection);
+    createPlots(newSelection);
+};
+
+//initial function on page load with sample data
+function init() {
+    //read json
+    d3.json("samples.json").then((data) => {
+    //filter to get names
+    console.log(data);
+    });
+
+};
